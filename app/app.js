1f44fc37b969413a29e8a9086c207c3cedb05246
Máté Pölyhe <matetu96@gmail.com>
2017-10-21 23:36:46 +0200

    sliderek alatt jó a szám

--- app/app.js
+++ app/app.js
@@ -6,3 +6,26 @@ App.controller('FunyiroAdatbazisCtrl', function($scope, $http) {
             $scope.funyirok = res.data;
         });
 });
+function refreshVagoszelesseg() {
+    var x = document.getElementById("vagoszelesseg_range").value;
+    document.getElementById("vagoszelessegCurrentValue").innerHTML = (x * 6/5).toString();
+
+}
+function refreshHengerurtartalom() {
+    var x = document.getElementById("hengerurtartalom_range").value;
+    document.getElementById("hengerurtartalomCurrentValue").innerHTML = (x * 6/5).toString();
+
+}
+function refreshTomeg() {
+    var x = document.getElementById("tomeg_range").value;
+    document.getElementById("tomegCurrentValue").innerHTML = (x * 6/5).toString();
+
+}
+function disableHajtasValaszto() {
+    document.getElementById("benzinHajtas").disabled = true;
+    document.getElementById("elektromosHajtas").disabled = true;
+}
+function enableHajtasValaszto() {
+    document.getElementById("benzinHajtas").disabled = false;
+    document.getElementById("elektromosHajtas").disabled = false;
+}
